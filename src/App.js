6b3ee308f19e98aa75d18acf03923de2034770ea
bengigenc <genc.bengi@gmail.com>
2022-08-19 12:28:19 +0300

    Add header and routes to books page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import Books from "./components/Books";
+import Book from "./components/Book";
+import Form from "./components/Form";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <p>hello bookstore project</p>
-    </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
