af4df567babb90004810fcb3b430fa741dba51ec
bengigenc <genc.bengi@gmail.com>
2022-08-19 14:16:29 +0300

    Fix linter issues

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,21 +4,27 @@ import Form from './Form';
 
 // since there is no data yet, using a hard coded array to present the functionality
 
-const Display = () => {
-  const data = [
-    {
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-  ];
-  data.map((book) => <Book key={1} title={book.title} author={book.author} />);
-};
+const Books = () => {
+  const Display = () => {
+    const data = [
+      {
+        title: 'Dune',
+        author: 'Frank Herbert',
+      },
+    ];
 
-const Books = () => (
-  <div>
-    <Display />
-    <Form />
-  </div>
-);
+    return (
+      data.map((book) => <Book key={1} title={book.title} author={book.author} />)
+
+    );
+  };
+
+  return (
+    <div>
+      <Display />
+      <Form />
+    </div>
+  );
+};
 
 export default Books;
