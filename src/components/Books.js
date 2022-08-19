cbe1412a3acd769b5fcccff0e7dc198a4bc3ed13
bengigenc <genc.bengi@gmail.com>
2022-08-19 11:51:21 +0300

    Create books component structure

---
+++ src/components/Books.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Book from "./Book";
+
+// since there is no data yet, using a hard coded array to present the functionality
+
+const Books = () => {
+  const data = [
+    {
+      title: "Dune",
+      author: "Frank Herbert",
+    },
+  ];
+
+  return data.map((book) => (
+    <Book key={1} title={book.title} author={book.author} />
+  ));
+};
+
+export default Books;
