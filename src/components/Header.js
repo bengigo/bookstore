68bd9aed3a3c89464d9c8903a1ff207024c420c5
bengigenc <genc.bengi@gmail.com>
2022-08-19 12:09:01 +0300

    Create header component structure

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
