e5de70f826771020bfa05a19c58ac92ba40e9d8a
bengigenc <genc.bengi@gmail.com>
2022-08-23 15:06:48 +0300

    Define book adding and removing action types

---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const initialState = [];
+
+export function addBook(book) {
+    return {
+        type: ADD_BOOK,
+        payload: book
+    };
+}
+
+export function removeBook(index) {
+    return {
+        type: REMOVE_BOOK,
+        payload: index
+    };
+}
+
+export default function bookReducer(state = initialState, action) {
+    switch(action.type) {
+        case ADD_BOOK:
+            return [...state, action.payload];
+        case REMOVE_BOOK:
+            return [...state.filter((book) => book !== action.payload)];
+        default:
+            return state;
+    }
+}
