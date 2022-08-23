aca491ba6ef2a695ca4d3fc4b4675c9c387ea0e7
bengigenc <genc.bengi@gmail.com>
2022-08-23 15:32:56 +0300

    Combine reducers into root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,9 @@
 import { createStore, combineReducers } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+    books: bookReducer,
+    categories: categoriesReducer,
+});
+
