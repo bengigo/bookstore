5593413362c99de3bf5d8c47f15cbe227de57014
bengigenc <genc.bengi@gmail.com>
2022-08-24 15:43:33 +0300

    Use redux in app components

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,29 +2,11 @@ import React from 'react';
 import Book from './Book';
 import Form from './Form';
 
-// since there is no data yet, using a hard coded array to present the functionality
-
-const Books = () => {
-  const Display = () => {
-    const data = [
-      {
-        title: 'Dune',
-        author: 'Frank Herbert',
-      },
-    ];
-
-    return (
-      data.map((book) => <Book key={1} title={book.title} author={book.author} />)
-
-    );
-  };
-
-  return (
-    <div>
-      <Display />
-      <Form />
-    </div>
-  );
-};
+const Books = () => (
+  <div>
+    <Book />
+    <Form />
+  </div>
+);
 
 export default Books;
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { statusCheck } from '../redux/categories/categories';
 
-const Categories = () => (
-  <button type="button">Check status</button>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories);
+
+  const handleClick = () => {
+    dispatch(statusCheck());
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={() => handleClick()}>
+        Check status
+      </button>
+      <p>{status}</p>
+    </div>
+  );
+};
 
 export default Categories;
