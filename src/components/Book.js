0fc9605a8d8de9320b5b169ed234261c9836849b
bengigenc <genc.bengi@gmail.com>
2022-08-26 13:31:28 +0300

    Style and position book list elements

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,11 +15,37 @@ const Book = () => {
     <ul>
       {Object.keys(data).map((book) => (
         <li key={book}>
-          <p>{data[book][0].title}</p>
-          <p>{data[book][0].author}</p>
-          <button type="button" onClick={() => handleClick(book)}>
-            Remove
-          </button>
+          <div className="summ">
+            <div className="details">
+              <p className="genre">Genre</p>
+              <p className="title">{data[book][0].title}</p>
+              <p className="author">{data[book][0].author}</p>
+            </div>
+            <div className="book-btns">
+              <button type="button" className="btn-book">
+                Comments
+              </button>
+              <button className="btn-book" type="button" onClick={() => handleClick(book)}>
+                Remove
+              </button>
+              <button type="button" className="btn-book">
+                Edit
+              </button>
+            </div>
+          </div>
+          <div className="progress">
+            <div className="Oval-2" />
+            <div className="status">
+              <p className="percentage">75%</p>
+              <p className="completed">Completed</p>
+            </div>
+          </div>
+          <div className="chapter">
+            <div className="current">CURRENT CHAPTER</div>
+            <div className="chapter-name">Lorem Ipsum</div>
+            <button type="button" className="btn-progress">UPDATE PROGRES</button>
+
+          </div>
         </li>
       ))}
     </ul>
